d68b4084cbbc9bc2a89187d26717950209cfdf3c
fizem <farid_izem@hotmail.com>
2023-01-31 19:48:14 +0100

    Initial commit

---
+++ src/main/java/com/fizem/inboxwatcher/InboxWatcherApplication.java
@@ -0,0 +1,13 @@
+package com.fizem.inboxwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxWatcherApplication.class, args);
+	}
+
+}
